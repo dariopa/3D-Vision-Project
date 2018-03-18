718e4ef1d5b81903a23f41eaae1d859260b33d07
Dario Panzuto <dariopa@users.noreply.github.com>
2018-03-18 15:45:41 +0100

    small correcture in the text
    -

--- Load_Data.m
+++ Load_Data.m
@@ -5,7 +5,7 @@ clear;	% Delete all variables.
 close all;	% Close all figure windows except those created by imtool.
 imtool close all;	% Close all figure windows created by imtool.
 
-%% Add library to create NPY outputs
+%% Add library for NIFTI_TOOLBOX
 addpath(genpath('NIFTI_TOOLBOX'));
 
 %% Define MyPath to our local Raw Data

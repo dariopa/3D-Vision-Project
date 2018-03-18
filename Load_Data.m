21a93355c7101debccc8767b3399db414d40c9a9
Dario Panzuto <dariopa@users.noreply.github.com>
2018-03-18 15:37:12 +0100

    Renamed Nifti_Toolbox, and furthermore added a file to load nifti images
    -

---
+++ Load_Data.m
@@ -0,0 +1,18 @@
+%% Load NIFTI Files
+
+clc;	% Clear command window.
+clear;	% Delete all variables.
+close all;	% Close all figure windows except those created by imtool.
+imtool close all;	% Close all figure windows created by imtool.
+
+%% Add library to create NPY outputs
+addpath(genpath('NIFTI_TOOLBOX'));
+
+%% Define MyPath to our local Raw Data
+MyPath = 'C:\Users\dario\Dropbox\3D Vision\Data';
+% MyPath = ;  % Define Cornels working folder
+% MyPath = ;  % Define Michelles working folder
+
+%% Import Nifti Images
+A = load_nii(fullfile([MyPath, '\patient001\patient001_4d.nii.gz']));
+B = load_nii(fullfile([MyPath, '\patient001\patient001_frame12.nii.gz']));

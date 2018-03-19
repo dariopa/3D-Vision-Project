ee88450d513931616d9a1a008fc37145d0f5a3a6
Michelle <qs.michelle.woon@gmail.com>
2018-03-19 13:47:52 +0100

    added viewing

--- Load_Data.m
+++ Load_Data.m
@@ -11,8 +11,11 @@ addpath(genpath('NIFTI_TOOLBOX'));
 %% Define MyPath to our local Raw Data
 MyPath = 'C:\Users\dario\Dropbox\3D Vision\Data';
 % MyPath = ;  % Define Cornels working folder
-% MyPath = ;  % Define Michelles working folder
+ MyPath = 'C:\Users\Saphi\Desktop\8th Semester\3D Vision\project\training';  % Define Michelles working folder
 
 %% Import Nifti Images
 A = load_nii(fullfile([MyPath, '\patient001\patient001_4d.nii.gz']));
 B = load_nii(fullfile([MyPath, '\patient001\patient001_frame12.nii.gz']));
+
+%% view
+view_nii(A)
